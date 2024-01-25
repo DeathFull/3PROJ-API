c879dfcd0a08d94980705e2086a8bf95853a0457
BriceA2103 <brice.aubineau@supinfo.com>
2024-01-25 15:31:48 +0100

    user CRUD without login and register

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,17 +1,37 @@
-import  { UserModel } from "../models/UserModel.js";
+import {UserModel} from "../models/UserModel.js";
 
 
 class UserRepository {
-    async listUsers() {
-        const user = await UserModel.find(
-            {},
-        {
-            firstname : true,
-            lastname : true,
-            email : true,
-            }
-        );
-        return user
-    }
+  async getUsers() {
+    const users = await UserModel.find(
+      {},
+      {
+        firstname: true,
+        lastname: true,
+        email: true,
+      }
+    );
+    return users
+  }
+
+  async getUserById(id) {
+    return await UserModel.findById(id);
+  }
+
+  async updateUser(id, payload) {
+    const newUser = await UserModel.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      payload
+    );
+
+    return newUser
+  }
+
+  async deleteUser(id) {
+    await UserModel.deleteOne({_id: id});
+  }
 }
+
 export default new UserRepository();
--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,19 +1,59 @@
 import express from "express";
-import passport from "passport";
-import { UserModel } from "../models/UserModel.js";
 import userRepository from "../repositories/UserRepository.js";
-import { z } from "zod";;
+import {z} from "zod";
+import UserRepository from "../repositories/UserRepository.js";
+
+;
 const router = express.Router();
 
-router.get("/",(req,res) => {
-
-});
-
 const UserRegisterSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
 });
+
+router.get("/", async (req, res) => {
+  const users = await userRepository.getUsers();
+  res.json(users)
+});
+
+router.get("/:id", async (req, res) => {
+  const {id} = req.params;
+  const user = await userRepository.getUserById(id);
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+  res.json(user);
+})
+
+router.post("/register", async (req, res) => {
+
+})
+
+
+router.post("/login", async (req, res) => {
+
+})
+
+
+router.put("/:id", async (req, res) => {
+  const {id} = req.params;
+  const userToUpdate = await userRepository.getUserById(id);
+  if (userToUpdate) {
+    await UserRepository.updateUser(id, req.body)
+  } else {
+    return res.status(404).send("User not found");
+  }
+  res.status(200).send("User updated");
+})
+
+
+router.delete("/:id", async (req, res) => {
+  const {id} = req.params;
+  await UserRepository.deleteUser(id);
+  res.status(204).send("User deleted")
+})
+
 
 
 
