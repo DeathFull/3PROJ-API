e3c83b303c3691169bbf59b72d7b34ae94ba5554
BriceA2103 <brice.aubineau@supinfo.com>
2024-05-17 05:23:23 +0200

    update balance

--- src/routers/BalanceRouter.js
+++ src/routers/BalanceRouter.js
@@ -39,7 +39,7 @@ balanceRouter.get("/user", loginMiddleware, async (req, res) => {
 
 balanceRouter.get("/group/:idGroup", loginMiddleware, async (req, res) => {
   const {idGroup} = req.params;
-  if (groupRepository.getGroupById(idGroup).members.includes(req.user) === false){
+  if ((await groupRepository.getGroupById(idGroup)).members.includes(req.user) === false){
     return res.status(403).send("You are not allowed to see this balance");
   }
   const balances = await balanceRepository.getBalancesByGroup(idGroup);
