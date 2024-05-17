aeb2e97e4a91f4a5a3aa9bf7b9ac1e7a28d8fb23
BriceA2103 <brice.aubineau@supinfo.com>
2024-05-17 04:42:04 +0200

    update balance and expense

--- src/routers/BalanceRouter.js
+++ src/routers/BalanceRouter.js
@@ -1,59 +1,86 @@
 import express from "express";
 import balanceRepository from "../repositories/BalanceRepository.js";
+import {loginMiddleware} from "../middlewares/loginMiddleware.js";
+import groupRepository from "../repositories/GroupRepository.js";
+import {z} from "zod";
 
 const balanceRouter = express.Router();
+
+const BalanceSchema = z.object({
+  idGroup: z.string(),
+  balance: z.number(),
+})
 
 balanceRouter.get("/", async (req, res) => {
   const balances = await balanceRepository.getBalances();
   return res.status(200).json(balances);
 })
 
-balanceRouter.get("/:id", async (req, res) => {
+balanceRouter.get("/:id", loginMiddleware, async (req, res) => {
   const {id} = req.params;
   const balance = await balanceRepository.getBalanceById(id)
   if (!balance) {
     return res.status(404).send("Balance not found");
   }
+  if (balance.idUser !== req.user) {
+    return res.status(403).send("You are not allowed to see this balance");
+  }
   return res.status(200).json(balance);
 })
 
 
-balanceRouter.get("/:idUser", async (req, res) => {
-  const {idUser} = req.params;
-  const balances = await balanceRepository.getBalancesByUser(idUser);
+balanceRouter.get("/user", loginMiddleware, async (req, res) => {
+  const balances = await balanceRepository.getBalancesByUser(req.user);
   if (!balances) {
     return res.status(404).send("Balances not found");
   }
   return res.status(200).json(balances);
 })
 
-balanceRouter.post("/", async (req, res) => {
+balanceRouter.get("/group/:idGroup", loginMiddleware, async (req, res) => {
+  const {idGroup} = req.params;
+  if (groupRepository.getGroupById(idGroup).members.includes(req.user) === false){
+    return res.status(403).send("You are not allowed to see this balance");
+  }
+  const balances = await balanceRepository.getBalancesByGroup(idGroup);
+  if (!balances) {
+    return res.status(404).send("Balances not found");
+  }
+  return res.status(200).json(balances);
+})
+
+balanceRouter.post("/", loginMiddleware, async (req, res) => {
   try {
-    const balance = await balanceRepository.createBalance(req.body);
+    if (req.body.idUser !== req.user) {
+      return res.status(403).send("You are not allowed to create a balance for another user");
+    }
+    const balanceData = {idUser: req.user, ...BalanceSchema.parse(req.body)}
+    const balance = await balanceRepository.createBalance(balanceData);
     return res.status(201).json(balance);
   } catch (e) {
     return res.status(400).send(e);
   }
 })
 
-balanceRouter.put("/:id", async (req, res) => {
+balanceRouter.put("/:id", loginMiddleware, async (req, res) => {
   try {
     const {id} = req.params;
     const balanceToUpdate = await balanceRepository.getBalanceById(id);
     if (!balanceToUpdate) {
       return res.status(404).send("Balance not found");
     }
-    await balanceRepository.updateBalance(id, req.body)
+    const balanceData = {idUser: req.user, ...BalanceSchema.parse(req.body)}
+    await balanceRepository.updateBalance(id, balanceData)
     return res.status(200).send("Balance updated")
   } catch (e) {
     return res.status(400).send(e);
   }
 })
 
-balanceRouter.delete("/:id", async (req, res) => {
-  const {id} = req.params;
-  await balanceRepository.deleteBalance(id);
-  return res.status(204).send("Balance deleted")
+balanceRouter.delete("/:idGroup", loginMiddleware, async (req, res) => {
+  const {idGroup} = req.params;
+  await balanceRepository.deleteBalance(idGroup, req.user);
+  return res.status(200).send("Balance deleted")
 })
 
 export default balanceRouter;
--- src/routers/ExpenseRouter.js
+++ src/routers/ExpenseRouter.js
@@ -1,7 +1,21 @@
 import express from "express";
 import expenseRepository from "../repositories/ExpenseRepository.js";
+import balanceRepository from "../repositories/BalanceRepository.js";
+import {loginMiddleware} from "../middlewares/loginMiddleware.js";
+import groupRepository from "../repositories/GroupRepository.js";
+import {z} from "zod";
 
 const expenseRouter = express.Router();
+
+const ExpenseSchema = z.object({
+  idGroup: z.string(),
+  name: z.string(),
+  description: z.string(),
+  amount: z.number(),
+  date: z.string(),
+  justification: z.string(),
+  category: z.string(),
+});
 
 expenseRouter.get("/", async (req, res) => {
   const expenses = await expenseRepository.getExpenses();
@@ -17,18 +31,20 @@ expenseRouter.get("/:id", async (req, res) => {
   return res.status(200).json(expense);
 });
 
-expenseRouter.get("/user/:idUser", async (req, res) => {
-  const {idUser} = req.params;
+expenseRouter.get("/user", loginMiddleware, async (req, res) => {
   const {sortBy, orderBy} = req.query;
-  const expenses = await expenseRepository.getExpensesByUser(idUser, sortBy, orderBy);
+  const expenses = await expenseRepository.getExpensesByUser(req.user, sortBy, orderBy);
   if (!expenses) {
     return res.status(404).send("Expenses not found");
   }
   return res.status(200).json(expenses);
 });
 
-expenseRouter.get("/group/:idGroup", async (req, res) => {
+expenseRouter.get("/group/:idGroup", loginMiddleware, async (req, res) => {
   const {idGroup} = req.params;
+  if (groupRepository.getGroupById(idGroup).members.includes(req.user) === false){
+    return res.status(403).send("You are not allowed to see this expense");
+  }
   const expenses = await expenseRepository.getExpensesByGroup(idGroup);
   if (!expenses) {
     return res.status(404).send("Expenses not found");
@@ -36,7 +52,7 @@ expenseRouter.get("/group/:idGroup", async (req, res) => {
   return res.status(200).json(expenses);
 });
 
-expenseRouter.get("/category/:category", async (req, res) => {
+expenseRouter.get("/category/:category", loginMiddleware, async (req, res) => {
   const {category} = req.params;
   const expenses = await expenseRepository.getExpensesByCategory(category);
   if (!expenses) {
@@ -45,33 +61,45 @@ expenseRouter.get("/category/:category", async (req, res) => {
   return res.status(200).json(expenses);
 });
 
-expenseRouter.post("/", async (req, res) => {
+expenseRouter.post("/", loginMiddleware, async (req, res) => {
   try {
-    const expense = await expenseRepository.createExpense(req.body);
+    /*if (req.body.idUser !== req.user) {
+      return res.status(403).send("You are not allowed to create an expense for another user");
+    }*/
+
+    const expenseData = {idUser: req.user, ...ExpenseSchema.parse(req.body)}
+    const expense = await expenseRepository.createExpense(expenseData);
+    if (expense) {
+      await balanceRepository.updateBalanceByUserAndGroup(expense.idUser, expense.idGroup, expense.amount);
+    }
     return res.status(201).json(expense);
   } catch (e) {
     return res.status(400).send(e);
   }
 });
 
-expenseRouter.put("/:id", async (req, res) => {
+expenseRouter.put("/:id", loginMiddleware, async (req, res) => {
   try {
     const {id} = req.params;
     const expenseToUpdate = await expenseRepository.getExpenseById(id);
     if (!expenseToUpdate) {
       return res.status(404).send("Expense not found");
     }
-    await expenseRepository.updateExpense(id, req.body);
+    const expenseData = {idUser: req.user, ...ExpenseSchema.parse(req.body)}
+    await expenseRepository.updateExpense(id, expenseData);
     return res.status(200).send("Expense updated");
   } catch (e) {
     return res.status(400).send(e);
   }
 });
 
-expenseRouter.delete("/:id", async (req, res) => {
-  const {id} = req.params;
-  await expenseRepository.deleteExpense(id);
-  return res.status(204).send("Expense deleted");
+expenseRouter.delete("/:idGroup", loginMiddleware, async (req, res) => {
+  const {idGroup} = req.params;
+  const expense = await expenseRepository.deleteExpense(idGroup, req.user);
+  if (expense) {
+    await balanceRepository.updateBalanceByUserAndGroup(expense.idUser, expense.idGroup, -expense.amount);
+  }
+  return res.status(200).send("Expense deleted");
 });
 
 export default expenseRouter;
