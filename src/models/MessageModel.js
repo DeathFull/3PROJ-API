fc2858a219f61f8b3968d7200caa701be450dd6d
DeathFull <contact@deathfull.net>
2024-05-19 04:27:10 +0200

    add: socket io chat

--- src/models/MessageModel.js
+++ src/models/MessageModel.js
@@ -1,10 +1,10 @@
-import mongoose, {Types} from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  senderId: {type: Types.ObjectId, ref: "User", required: true},
-  receiverId: {type: Types.ObjectId, ref: "User", required: true},
-  groupId: {type: Types.ObjectId, ref: "Group", required: true},
-  message: {type: String, required: true},
+  senderId: { type: Types.ObjectId, ref: "User", required: true },
+  receiverId: { type: Types.ObjectId, ref: "User" },
+  groupId: { type: Types.ObjectId, ref: "Group" },
+  message: { type: String, required: true },
 });
 
 export const MessageModel = mongoose.model("Message", messageSchema);
