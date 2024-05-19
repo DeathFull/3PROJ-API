59d6c6578931cf58d690aff988650c5de3b1fa6c
DeathFull <contact@deathfull.net>
2024-05-19 02:05:30 +0200

    add: debt model

--- src/models/RefundModel.js
+++ src/models/RefundModel.js
@@ -4,8 +4,8 @@ const refundSchema = new mongoose.Schema({
   payerId: { type: Types.ObjectId, ref: "User", required: true },
   refunderId: { type: Types.ObjectId, ref: "User", required: true },
   idGroup: { type: Types.ObjectId, ref: "Group", required: true },
-  amount: { type: Number, default: 0},
-  isRefunded: { type: Boolean, default: false },
+  amount: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 export const RefundModel = mongoose.model("Refund", refundSchema);
